e781177d83ac854f1215ac91bf5d7c46e05787aa
ZimGil <gil.tichon@gmail.com>
2018-05-26 22:14:34 +0300

    Added textbox for path

--- src/app/serve/serve.component.ts
+++ src/app/serve/serve.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-serve',
@@ -10,17 +11,21 @@ export class ServeComponent {
   private readonly MINIMUM_PORT = 1000;
   private readonly MAXIMUM_PORT = 99999;
 
+  rootFolder = "C:\\Development";
   hostname = "localhost";
   port = 4200;
   isOpen = true;
+  isForwardslash = false;
 
   runServer(): void {
+    this.forwardSlashToBackslash();
     const request = {
       hostname: this.hostname,
       port: this.port,
-      isOpen: this.isOpen
+      isOpen: this.isOpen,
+      rootFolder: this.rootFolder
     }
-    console.log('request: ', request);  
+    console.log('request: ', request);
   }
 
   isPortValid(): boolean {
@@ -31,8 +36,30 @@ export class ServeComponent {
     return !!this.hostname;
   }
 
+  isRootFolderValid(): boolean {
+    if (this.isFirstCharLetter() && this.rootFolder[1] == ":" && this.isBackslash()) {
+      return true;
+    }
+    return false;
+  }
+
+  isFirstCharLetter(): boolean {
+    return ((this.rootFolder[0] >= 'a' && this.rootFolder[0] <= 'z') || (this.rootFolder[0] >= 'A' && this.rootFolder[0] <= 'Z'));
+  }
+
+  isBackslash(): boolean {
+    if ((this.rootFolder[2] == "/") || (this.rootFolder[2] == "\\")) {
+      return true;
+    }
+    return false;
+  }
+
+  forwardSlashToBackslash() {
+    this.rootFolder = this.rootFolder.replace(/\//g,"\\");
+  }
+
   isAllInputsValid(): boolean {
-    return this.isHostnameValid() && this.isPortValid();
+    return this.isHostnameValid() && this.isPortValid() && this.isRootFolderValid();
   }
 
 }
