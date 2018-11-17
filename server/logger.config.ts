98671928d5f88df2e7ad2047e87f2f4dcfdf5006
ZimGil <gil.tichon@gmail.com>
2018-11-17 17:09:55 +0200

    deleting 1 week old files

--- server/logger.config.ts
+++ server/logger.config.ts
@@ -1,11 +1,18 @@
 import { configure } from "log4js";
 import { getAppDataPath } from "appdata-path";
+//
 import moment = require('moment');
+import fs = require('fs');
+import path = require('path');
+
+const LOG_FILES_DIR = getAppDataPath(`ngWiz${path.sep}logs`);
 
 export function loggerConfig(): void {
 
     const dateInFormat = moment().format('DDMMYYTHHmmZZ'); // [171118T1510+0200]
-    const logFileName = `${getAppDataPath('ngWiz')}/logs/[${dateInFormat}].debug.log`;
+    const logFileName = `${LOG_FILES_DIR}/[${dateInFormat}].debug.log`;
+
+    deleteOldLogs();
 
     configure({
         appenders: {
@@ -19,4 +26,22 @@ export function loggerConfig(): void {
           default: { appenders: [ 'fileFilter', 'stderrFilter', 'stdout' ], level: 'DEBUG' }
         }
       });
+}
+
+function deleteOldLogs(): void {
+  fs.readdir(LOG_FILES_DIR, (err, files) => {
+    files.forEach(file => {
+      
+      if (path.extname(file) === '.log'){
+        let filePath = LOG_FILES_DIR + path.sep + file;
+        fs.stat(filePath, (err, stats) => {
+          let lastModify = moment(stats.mtime);
+
+          if (lastModify.isBefore(moment().subtract(1, 'w'))) {
+            fs.unlink(filePath, err => {});
+          }
+        })
+      }
+    })
+  })
 }
